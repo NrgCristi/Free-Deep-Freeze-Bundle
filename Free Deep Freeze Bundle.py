3396d34785064e5642a86b1cd80edca26b4a714a
CristianCosticea <97525096+CristianCosticea@users.noreply.github.com>
2022-04-12 13:47:51 +0300

    Add files via upload

---
+++ Free Deep Freeze Bundle.py
@@ -0,0 +1,4 @@
+mesajstart = ("Downloading Everything You Need")
+print(mesajstart)
+mesajstop = ("Files Downloaded, Now You Only Need To Keep This Opened And Then Open Fortnite And Then You Will Get The Deep Freeze Bundle For Free")
+print(mesajstop)
