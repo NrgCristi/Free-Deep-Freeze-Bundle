b60beb824b52fea03d555b70762ba261a54044cd
CristianCosticea <97525096+CristianCosticea@users.noreply.github.com>
2022-04-12 13:47:15 +0300

    Delete Free Deep Freeze Bundle.py

--- Free Deep Freeze Bundle.py
+++
@@ -1,4 +0,0 @@
-mesajstart = ("Downloading Everything You Need")
-print(mesajstart)
-mesajstop = ("Files Downloaded, Now You Only Need To Keep This Opened And You Will Get The Deep Freeze Bundle For Free")
-print(mesajstop)
